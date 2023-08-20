033d4a7e3fbaf4f3a567c687a3b322ad3a2018be
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-08-20 20:02:46 +0600

    navbar and footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { AuthProvider } from '@/context/authContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/header/Navbar'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+          <Navbar></Navbar>
         <AuthProvider>
           {children} 
         </AuthProvider>
+          <Footer></Footer>
        
         
         </body>
